d6ad54abe74004747d33f88faaf993c2a5c92873
Richard Goulter <richard.goulter@gmail.com>
2015-09-02 16:42:04 +0800

    Deal with Scala syntax here, to remove `new Tuple2(...)`

--- src/test/scala/systemTestingDSL/InferenceTesterTest.scala
+++ src/test/scala/systemTestingDSL/InferenceTesterTest.scala
@@ -40,45 +40,45 @@ class InferenceTesterTest {
       val expectedResult = new MutableList[(String, String)]
 
       // Entail 1
-      expectedResult += new Tuple2("Valid", """<1>true&q=null & n=inf_n_27 & inf_ann_26<=0&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&q=null & n=inf_n_27 & inf_ann_26<=0&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_27>@inf_ann_26[Orig][LHSCase]]
- inferred pure: [inf_ann_26<=0]""")
+ inferred pure: [inf_ann_26<=0]"""))
 
       // Entail 2
-      expectedResult += new Tuple2("Valid", """
+      expectedResult += (("Valid", """
 	 <1>true&q=null & n=0&{FLOW,(17,18)=__norm}[]
- inferred pure: [n=0]""")
+ inferred pure: [n=0]"""))
 
       // Entail 3
-      expectedResult += new Tuple2("Valid", """<1>true&q!=null & n=inf_n_42 & inf_ann_41<=0&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&q!=null & n=inf_n_42 & inf_ann_41<=0&{FLOW,(17,18)=__norm}[]
 	 inferred heap: [q::ll<inf_n_42>@inf_ann_41[Orig][LHSCase]]
-	 inferred pure: [inf_ann_41<=0]""")
+	 inferred pure: [inf_ann_41<=0]"""))
 
       // Entail 4
-      expectedResult += new Tuple2("Fail", """""")
+      expectedResult += (("Fail", """"""))
 
       // Entail 5
-      expectedResult += new Tuple2("Valid", """<1>true&n=0 & inf_ann_60<=0 & inf_n_61<=0&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&n=0 & inf_ann_60<=0 & inf_n_61<=0&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_61>@inf_ann_60[Orig][LHSCase]]
- inferred pure: [inf_n_61<=0; inf_ann_60<=0]""")
+ inferred pure: [inf_n_61<=0; inf_ann_60<=0]"""))
 
       // Entail 6
-      expectedResult += new Tuple2("Valid", """<1>false&false&{FLOW,(17,18)=__norm}[]
- inferred pure: [n!=0]""")
+      expectedResult += (("Valid", """<1>false&false&{FLOW,(17,18)=__norm}[]
+ inferred pure: [n!=0]"""))
 
       // Entail 7
-      expectedResult += new Tuple2("Valid", """<1>true&n=0 & inf_ann_85<=0 & n=inf_n_86&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&n=0 & inf_ann_85<=0 & n=inf_n_86&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_86>@inf_ann_85[Orig][LHSCase]]
- inferred pure: [n=inf_n_86; inf_ann_85<=0]""")
+ inferred pure: [n=inf_n_86; inf_ann_85<=0]"""))
 
       // Entail 8
-      expectedResult += new Tuple2("Valid", """<1>true&n=0 & inf_ann_99<=0 & n=inf_n_100&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&n=0 & inf_ann_99<=0 & n=inf_n_100&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_100>@inf_ann_99[Orig][LHSCase]]
- inferred pure: [n=inf_n_100; inf_ann_99<=0]""")
+ inferred pure: [n=inf_n_100; inf_ann_99<=0]"""))
 
       // Entail 9
-      expectedResult += new Tuple2("Valid", """<1>false&false&{FLOW,(17,18)=__norm}[]
- inferred pure: [n=0]""")
+      expectedResult += (("Valid", """<1>false&false&{FLOW,(17,18)=__norm}[]
+ inferred pure: [n=0]"""))
 
       assertTrue(checkCorpus(InferenceTestData.infer2Output, expectedResult))
     }
@@ -91,49 +91,49 @@ class InferenceTesterTest {
       val expectedResult = new ArrayBuffer[(String, String)]
 
       // Entail 1
-      expectedResult += new Tuple2("Valid", """ <1>true&q=null & n=inf_n_27 & inf_ann_26<=0&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """ <1>true&q=null & n=inf_n_27 & inf_ann_26<=0&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_27>@inf_ann_26[Orig][LHSCase]]
- inferred pure: [inf_ann_26<=0]""")
+ inferred pure: [inf_ann_26<=0]"""))
 
       // Entail 2
-      expectedResult += new Tuple2("Valid", """
+      expectedResult += (("Valid", """
     <1>true&q=null & n=0&{FLOW,(17,18)=__norm}[]
- inferred pure: [n=0]""")
+ inferred pure: [n=0]"""))
 
       // Entail 3
-      expectedResult += new Tuple2("Valid", """
+      expectedResult += (("Valid", """
     <1>true&q!=null & n=inf_n_42 & inf_ann_41<=0&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_42>@inf_ann_41[Orig][LHSCase]]
- inferred pure: [inf_ann_41<=0]""")
+ inferred pure: [inf_ann_41<=0]"""))
 
       // Entail 4
-      expectedResult += new Tuple2("Fail", """""")
+      expectedResult += (("Fail", """"""))
 
       // Entail 5
-      expectedResult += new Tuple2("Valid", """
+      expectedResult += (("Valid", """
     <1>true&n=0 & inf_ann_60<=0 & inf_n_61<=0&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_61>@inf_ann_60[Orig][LHSCase]]
- inferred pure: [inf_n_61<=0; inf_ann_60<=0]""")
+ inferred pure: [inf_n_61<=0; inf_ann_60<=0]"""))
 
       // Entail 6
-      expectedResult += new Tuple2("Valid", """
+      expectedResult += (("Valid", """
     <1>false&false&{FLOW,(17,18)=__norm}[]
- inferred pure: [n!=0]""")
+ inferred pure: [n!=0]"""))
 
       // Entail 7
-      expectedResult += new Tuple2("Valid", """<1>true&n=0 & inf_ann_85<=0 & n=inf_n_86&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&n=0 & inf_ann_85<=0 & n=inf_n_86&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_86>@inf_ann_85[Orig][LHSCase]]
- inferred pure: [n=inf_n_86; inf_ann_85<=0]""")
+ inferred pure: [n=inf_n_86; inf_ann_85<=0]"""))
 
       // Entail 8
-      expectedResult += new Tuple2("Valid", """<1>true&n=0 & inf_ann_99<=0 & n=inf_n_100&{FLOW,(17,18)=__norm}[]
+      expectedResult += (("Valid", """<1>true&n=0 & inf_ann_99<=0 & n=inf_n_100&{FLOW,(17,18)=__norm}[]
  inferred heap: [q::ll<inf_n_100>@inf_ann_99[Orig][LHSCase]]
- inferred pure: [n=inf_n_100; inf_ann_99<=0]""")
+ inferred pure: [n=inf_n_100; inf_ann_99<=0]"""))
 
       // Entail 9
-      expectedResult += new Tuple2("Valid", """
+      expectedResult += (("Valid", """
     <1>false&false&{FLOW,(17,18)=__norm}[]
- inferred pure: [n=0]""")
+ inferred pure: [n=0]"""))
 
       assertFalse(checkCorpus(InferenceTestData.infer1Output, expectedResult))
     }
@@ -143,6 +143,6 @@ class InferenceTesterTest {
   def infer4Test(): Unit = {
     val expectedResult = new ArrayBuffer[(String, String)]
 
-    expectedResult += new Tuple2("Valid", """""")
+    expectedResult += (("Valid", """"""))
   }
 }
