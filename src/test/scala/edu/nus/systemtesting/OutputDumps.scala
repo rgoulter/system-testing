a21b25f0f2c1b4c28079b5daa0c8176f5513f314
Richard Goulter <richard.goulter@gmail.com>
2015-09-07 19:07:23 +0800

    Add dump of some Hip output

--- src/test/scala/edu/nus/systemtesting/OutputDumps.scala
+++ src/test/scala/edu/nus/systemtesting/OutputDumps.scala
@@ -20,4 +20,7 @@ object OutputDumps {
   // For `sleek examples/working/sleek/sleek.slk`,
   // expected output is "Valid, Valid, Valid, Fail"
   lazy val SleekExResource = loadResource("outputdump/sleek.sleek.slk.soccf-plser2-05.7ac44bdb0dfd.txt")
+
+  // expected output is "remove: SUCCESS, append: SUCCESS"
+  lazy val HipExResource = loadResource("outputdump/hip.--dsd_--en-inf.inflist.ss.soccf-plser2-05.7ac44bdb0dfd.txt")
 }
