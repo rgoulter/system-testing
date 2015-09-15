41e9691c3673b461b02944737fe0cfd6d35aeddc
Richard Goulter <richard.goulter@gmail.com>
2015-09-15 15:15:00 +0800

    Main to delete tmp archive folder after running the tests

--- src/main/scala/edu/nus/systemtesting/hipsleek/Main.scala
+++ src/main/scala/edu/nus/systemtesting/hipsleek/Main.scala
@@ -31,82 +31,109 @@ object Main {
     }
   }
 
-  private def prepareRepo(repoDir : String, rev : Option[String]): (Boolean, String, String) = {
+  private def runTestsWith(repoDir: String, rev: Option[String])(f: (String, String) => Unit): Unit = {
     val isRepo = Paths.get(repoDir, ".hg").toFile().exists()
 
     if (isRepo) {
-      // Prepare the repo, if necessary
-      println("Preparing repo...")
-
-      val repo = new Repository(repoDir)
-      val revision = rev.getOrElse(repo.identify())
-
-      val isDirty = rev match {
-        // Assumes that given rev isn't a "dirty" one;
-        // e.g. a call to "hg update <rev>" would make sense
-        case Some(s) => false
-        // If no rev given, use Working Directory of repo.
-        case None => repo.isDirty()
-      }
-
-      val tmpDir = Files.createTempDirectory("edunussystest")
-
-      val projectDir = if (isDirty) {
-        // i.e. LIVE, "in place"
-        repoDir
-      } else {
-          val tmp = tmpDir.toAbsolutePath().toString()
-          repo.archive(tmp, rev) // create archive of repo in tmp
-          tmp
-      }
-
-      val prep = new HipSleekPreparation(projectDir)
-      val (prepWorked, prepRemarks) = prep.prepare()
-
-      prepRemarks.foreach(println)
-      println
-
-      // TODO: Unfortunately, this means we don't delete the tmp dir
-      // after running the tests.
-
-      (prepWorked, projectDir, revision)
+      runTestsWithRepo(repoDir, rev)(f)
     } else {
-      // i.e. LIVE, "in place"
-      val projectDir = repoDir
-      val revision = rev.getOrElse("unknown")
-
-      // Prepare the repo, if necessary
-      println("Preparing folder...")
-
-      val prep = new HipSleekPreparation(projectDir)
-      val (prepWorked, prepRemarks) = prep.prepare()
-
-      prepRemarks.foreach(println)
-      println
-
-      (prepWorked, projectDir, revision)
+      runTestsWithFolder(repoDir, rev)(f)
     }
   }
 
-  private def runAllTests(repoDir : String, rev : Option[String]): Unit = {
-    val (prepWorked, projectDir, revision) = prepareRepo(repoDir, rev)
+  /**
+   * `repoDir` is assumed to be a repository. This method creates an archive
+   * of the repo in a tmp directory if the revision is specified, or
+   * if the repository is 'clean'.
+   *
+   * If the repository is dirty, the repository's working directory is used
+   * to run, and it is assumed that this folder can be used to make, and
+   * run the tests in.
+   */
+  private def runTestsWithRepo(repoDir: String, rev: Option[String])(f: (String, String) => Unit): Unit = {
+    // Prepare the repo, if necessary
+    println("Preparing repo...")
 
-    if (!prepWorked)
-      return
+    val repo = new Repository(repoDir)
+    val revision = rev.getOrElse(repo.identify())
 
-    runPreparedSleekTests(projectDir, revision)
-    runPreparedHipTests(projectDir, revision)
+    val isDirty = rev match {
+      // Assumes that given rev isn't a "dirty" one;
+      // e.g. a call to "hg update <rev>" would make sense
+      case Some(s) => false
+      // If no rev given, use Working Directory of repo.
+      case None => repo.isDirty()
+    }
+
+    val tmpDir = Files.createTempDirectory("edunussystest")
+
+    val projectDir = if (isDirty) {
+      // i.e. LIVE, "in place"
+      repoDir
+    } else {
+        val tmp = tmpDir.toAbsolutePath().toString()
+
+        // create archive of repo in tmp
+        repo.archive(tmp, rev)
+
+        tmp
+    }
+
+    val prep = new HipSleekPreparation(projectDir)
+    val (prepWorked, prepRemarks) = prep.prepare()
+
+    prepRemarks.foreach(println)
+    println
+
+    // Run the tests
+    if (prepWorked)
+      f(projectDir, revision)
+
+    // Finished running the tests, clean up.
+    tmpDir.toFile().delete()
   }
 
-  private def runSleekTests(repoDir : String, rev : Option[String]): Unit = {
-    val (prepWorked, projectDir, revision) = prepareRepo(repoDir, rev)
+  /**
+   * `projectDir` not assumed to be repository.
+   * It *is* assumed that `projectDir` will be used for making, running the
+   * executables/tests.
+   */
+  private def runTestsWithFolder(projectDir: String, rev: Option[String])(f: (String, String) => Unit): Unit = {
+    // i.e. LIVE, "in place"
+    val revision = rev.getOrElse("unknown")
 
-    if (!prepWorked)
-      return
+    // Prepare the repo, if necessary
+    println("Preparing folder...")
 
-    runPreparedSleekTests(projectDir, revision)
+    val prep = new HipSleekPreparation(projectDir)
+    val (prepWorked, prepRemarks) = prep.prepare()
+
+    prepRemarks.foreach(println)
+    println
+
+    // Run the tests
+    if (prepWorked)
+      f(projectDir, revision)
+
+    (prepWorked, projectDir, revision)
   }
 
+  private def runAllTests(repoDir: String, rev: Option[String]): Unit = {
+    runTestsWith(repoDir, rev) { (projectDir, revision) =>
+      runPreparedSleekTests(projectDir, revision)
+      runPreparedHipTests(projectDir, revision)
+    }
+  }
+
+  private def runSleekTests(repoDir: String, rev: Option[String]): Unit = {
+    runTestsWith(repoDir, rev)(runPreparedSleekTests)
+  }
+
+  private def runHipTests(repoDir: String, rev: Option[String]): Unit = {
+    runTestsWith(repoDir, rev)(runPreparedHipTests)
+  }
+
+  /** Assumes that the project dir has been prepared successfully */
   private def runPreparedSleekTests(projectDir: String, revision: String): Unit = {
     val config = ConfigFactory.load()
 
@@ -116,15 +143,7 @@ object Main {
     new SleekTestSuiteUsage(config, command, examples, revision).run()
   }
 
-  private def runHipTests(repoDir : String, rev : Option[String]): Unit = {
-    val (prepWorked, projectDir, revision) = prepareRepo(repoDir, rev)
-
-    if (!prepWorked)
-      return
-
-    runPreparedSleekTests(projectDir, revision)
-  }
-
+  /** Assumes that the project dir has been prepared successfully */
   private def runPreparedHipTests(projectDir: String, revision: String): Unit = {
     val config = ConfigFactory.load()
 
