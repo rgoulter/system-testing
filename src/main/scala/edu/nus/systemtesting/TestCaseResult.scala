d95f4bac8862ee5138479627637529e7308d39ba
Richard Goulter <richard.goulter@gmail.com>
2015-09-18 15:28:25 +0800

    Logic for TestSuiteComparison, computing diff of TestSuiteResult

--- src/main/scala/edu/nus/systemtesting/TestCaseResult.scala
+++ src/main/scala/edu/nus/systemtesting/TestCaseResult.scala
@@ -36,4 +36,31 @@ case class TestCaseResult(val command: Path,
     // Doesn't make sense to ask for remarks in case of Right
     case Right(_) => Array[String]()
   }
+
+  val executionSucceeded = results.isRight
+
+  val passed = result == TestPassed
+
+  /** Equivalence val, so that `" --a --b"` is the same as `"--b --a"`. */
+  lazy val sortedArgs =
+    (arguments trim() split "\\s*" sorted) mkString " "
+
+  def expected() = results match {
+    case Left(_) => List[String]()
+    case Right(results) => results map(_.expected) toList
+  }
+
+  /**
+   * Key which almost certain to be unique to a [[TestCaseResult]] in a
+   * [[TestSuite]]
+   */
+  lazy val cmdFnArgsKey =
+    (command, filename, sortedArgs)
+
+  /**
+   * Key which might be unique to a [[TestCaseResult]] in a [[TestSuite]].
+   * Useful for finding tests which differ only in `arguments`.
+   */
+  lazy val cmdFnKey =
+    (command, filename, sortedArgs)
 }
--- src/main/scala/edu/nus/systemtesting/testsuite/TestSuiteComparison.scala
+++ src/main/scala/edu/nus/systemtesting/testsuite/TestSuiteComparison.scala
@@ -14,7 +14,7 @@ class TestSuiteComparison() {
 }
 
 object TestSuiteComparison {
-  def apply(oldTS: TestSuiteResult, currTS: TestSuiteResult) {
+  def apply(oldTS: TestSuiteResult, curTS: TestSuiteResult) {
     // It can't be assumed that oldTS, currTS ran the same set of TestCases
     // (in terms of *SuiteUsage), since these can change over time as TestCases
     // are added, program flags change, etc.
@@ -25,37 +25,84 @@ object TestSuiteComparison {
     // (but with same set of files) and it'd work just fine, too.
 
     val oldRevision = oldTS.repoRevision
-    val currRevision = currTS.repoRevision
+    val curRevision = curTS.repoRevision
 
     //
     // Find which TestCaseResults can be compared, which can't
     //
 
     // Build Map[(cmd,filename,sortedArgs) => TestCaseResult)] for old,curr
+    val oldTests = oldTS.results.toList
+    val curTests = curTS.results.toList
 
-    // Partition into:
-    // * compatible (old,curr)
-    // * unmatched old,
-    // * unused curr
+    val oldTestMap = oldTests.map(tcr => (tcr.cmdFnArgsKey -> tcr)).toMap
+    val curTestMap = curTests.map(tcr => (tcr.cmdFnArgsKey -> tcr)).toMap
 
-    // DiffArgs for those with same (cmd,filename) in unmatched old, unused curr
-    //   (assuming that (cmd,filename) is unique-ish)
-    // TestRemoved if (cmd,filename) in old, not in curr,
-    // NewTest     if (cmd,filename) not in old, in curr
+    // Partition (Compatible TestCaseResults)
+    val compatibleTests = oldTests.map({ oldTCR =>
+      curTestMap.get(oldTCR.cmdFnArgsKey).map(curTCR => (oldTCR -> curTCR))
+    }).flatten
 
-    // For the compatible,
+    val (usedOld, usedCur) = compatibleTests.unzip
+    val unusedOld = oldTests.filterNot(usedOld.contains)
+    val unusedCur = curTests.filterNot(usedCur.contains)
+
+    val unusedOldMap = unusedOld.map(tcr => (tcr.cmdFnKey -> tcr)).toMap
+    val unusedCurMap = unusedCur.map(tcr => (tcr.cmdFnKey -> tcr)).toMap
+
+    // Partition Unused (Different Args)
+    val argsChangedTests = unusedOld.map({ oldTCR =>
+      // assumes cmdFn key is unique-ish,
+      // otherwise `argsChangedTests` is nonsense
+      unusedCurMap.get(oldTCR.cmdFnKey).map(curTCR => (oldTCR -> curTCR))
+    }).flatten
+
+    // Test is 'removed' if (cmd,file) pair not in new tests.
+    // Test is 'new' if (cmd,file) pair not in old tests.
+    // (Otherwise, with same args in compatible tests,
+    //  with different args in argsChangedTests).
+    val (argsChangedOld, argsChangedCur) = argsChangedTests.unzip
+    val removedTests = unusedOld.filterNot(argsChangedOld.contains)
+    val newTests     = unusedCur.filterNot(argsChangedCur.contains)
+
+    //
+    // For the compatible tests:
+    //
 
     // With each (old,curr) TestCaseResult:
     // If both ran successfully AND
     //   if #/seq of Result.expected is different, => ExpectedChanged
+    val (successfulPairs, unsuccessfulPairs) = compatibleTests.partition({ case (oldTCR, curTCR) =>
+      oldTCR.executionSucceeded && curTCR.executionSucceeded
+    })
 
-    // Otherwise, the DIFFERENT results will be:
-    // (failedToRun, successfullyRun) # GOOD
-    // (successfullyRun, failedToRun) # BAD
-    // (testcase failed, testcase passed) # GOOD
-    // (testcase passed, testcase failed) # BAD
+    // 'comparable' stronger than 'compatible'; it implies same expected.
+    val (comparableTests, diffExpectedTests) = successfulPairs.partition({ case (oldTCR, curTCR) =>
+      oldTCR.expected == curTCR.expected
+    })
 
-    // What remains is "not different";
-    // Can print summary of the currRes if need be (to see #passed, #failed)
+    // Change in successfully ran?
+    val nowSuccessfullyRuns = unsuccessfulPairs.filter({ case (oldTCR, curTCR) =>
+      !oldTCR.executionSucceeded && curTCR.executionSucceeded
+    })
+    val usedToSuccessfullyRun = unsuccessfulPairs.filter({ case (oldTCR, curTCR) =>
+      oldTCR.executionSucceeded && !curTCR.executionSucceeded
+    })
+    // Remaining (from unsuccessfulPairs): both fail to run.
+
+    // Change in test passing?
+    val nowPasses = comparableTests.filter({ case (oldTCR, curTCR) =>
+      !oldTCR.passed && curTCR.passed
+    })
+    val usedToPass = comparableTests.filter({ case (oldTCR, curTCR) =>
+      oldTCR.passed && !curTCR.passed
+    })
+    // Remaining (from comparableTests): results both 'pass' or both 'fail',
+    // May be that they fail for different reasons:
+    val diffDiffs = comparableTests.filter({case (oldTCR, curTCR) =>
+      (!oldTCR.passed && !curTCR.passed) && (oldTCR.diff != curTCR.diff)
+    })
+
+    // Now construct TestSuiteComparison from the above
   }
 }
