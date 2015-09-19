8d7770422fd52f16c93a7c2a0178417e3432a1f0
Richard Goulter <richard.goulter@gmail.com>
2015-09-19 18:49:58 +0800

    Move displayResult() from TestSuite to TestCaseResult

--- src/main/scala/edu/nus/systemtesting/TestCaseResult.scala
+++ src/main/scala/edu/nus/systemtesting/TestCaseResult.scala
@@ -2,7 +2,9 @@ package edu.nus.systemtesting
 
 import java.nio.file.Path
 
-import java.nio.file.Path
+import edu.nus.systemtesting.output.GlobalReporter
+import edu.nus.systemtesting.output.ReporterColors._
+import GlobalReporter.reporter
 
 /**
  * A system level test will either Pass or Fail.
@@ -63,4 +65,59 @@ case class TestCaseResult(val command: Path,
    */
   lazy val cmdFnKey =
     (command, filename, sortedArgs)
+
+  /**
+   * @param threshold the min value `executionTime` needs to be to be shown.
+   */
+  def displayResult(threshold: Long = 1000L) = {
+    // Assuming that execCmd is of same form as run in Runnable
+    // Output (cmd, args?, filename) relative to projectDir, corpusDir
+    val trimmedArgs = arguments.trim()
+    val execCmd =
+      if (trimmedArgs.isEmpty())
+        Seq(command, filename).mkString(" ")
+      else
+        Seq(command, trimmedArgs, filename).mkString(" ")
+
+    reporter.print(execCmd)
+
+    // Ensure the "Passed"/"Failed" result is printed on ResultCol
+    val ResultCol = 70
+    val pad = ResultCol - execCmd.length()
+    reporter.print(if (pad >= 0) {" " * pad} else {"\n" + " " * ResultCol})
+
+    val resStr = result match {
+      case TestPassed => reporter.inColor(ColorGreen)("Passed")
+      case TestFailed => reporter.inColor(ColorRed  )("Failed")
+    }
+
+    reporter.println(resStr)
+
+
+    if (executionTime > threshold) {
+      reporter.log(s"Runtime: $executionTime milliseconds")
+    }
+
+
+    def expect(m: String) = reporter.inColor(ColorCyan)(m)
+    def actual(m: String) = reporter.inColor(ColorMagenta)(m)
+
+    results match {
+      case Left(remarks) => {
+        remarks.foreach(reporter.log)
+
+        reporter.println()
+      }
+      case Right(results) => {
+        val diff = results.filterNot(_.passed)
+
+        diff.foreach({ case Result(key, expected, got) =>
+          reporter.println(s"Expected ${expect(expected)}, but got ${actual(got)} for $key")
+        })
+
+        if (!diff.isEmpty)
+          reporter.println()
+      }
+    }
+  }
 }
--- src/main/scala/edu/nus/systemtesting/testsuite/TestSuite.scala
+++ src/main/scala/edu/nus/systemtesting/testsuite/TestSuite.scala
@@ -8,7 +8,6 @@ import org.joda.time.DateTime
 import edu.nus.systemtesting.{ Result, TestCase,
                                TestCaseResult, TestFailed, TestPassed }
 import edu.nus.systemtesting.output.GlobalReporter
-import edu.nus.systemtesting.output.ReporterColors._
 import GlobalReporter.reporter
 
 class TestSuite(tests: List[TestCase],
@@ -23,7 +22,7 @@ class TestSuite(tests: List[TestCase],
     val testResults = tests.map(test => {
       val testResult = test.generateOutput
 
-      displayResult(testResult)
+      testResult.displayResult(THRESHOLD)
 
       testResult
     })
@@ -42,59 +41,5 @@ class TestSuite(tests: List[TestCase],
     val suiteResult = TestSuiteResult(hostname, now, revision, testResults)
 
     suiteResult
-  }
-
-  def displayResult(result: TestCaseResult) = {
-    // Assuming that execCmd is of same form as run in Runnable
-    // Output (cmd, args?, filename) relative to projectDir, corpusDir
-    val trimmedArgs = result.arguments.trim()
-    val execCmd =
-      if (trimmedArgs.isEmpty())
-        Seq(result.command, result.filename).mkString(" ")
-      else
-        Seq(result.command, trimmedArgs, result.filename).mkString(" ")
-
-    reporter.print(execCmd)
-
-    // Ensure the "Passed"/"Failed" result is printed on ResultCol
-    val ResultCol = 70
-    val pad = ResultCol - execCmd.length()
-    reporter.print(if (pad >= 0) {" " * pad} else {"\n" + " " * ResultCol})
-
-    val resStr = result.result match {
-      case TestPassed => reporter.inColor(ColorGreen)("Passed")
-      case TestFailed => reporter.inColor(ColorRed  )("Failed")
-    }
-
-    reporter.println(resStr)
-
-
-    val time = result.executionTime
-
-    if (time > THRESHOLD) {
-      reporter.log("Runtime: " + time + " milliseconds")
-    }
-
-
-    def expect(m: String) = reporter.inColor(ColorCyan)(m)
-    def actual(m: String) = reporter.inColor(ColorMagenta)(m)
-
-    result.results match {
-      case Left(remarks) => {
-        remarks.foreach(reporter.log)
-
-        reporter.println()
-      }
-      case Right(results) => {
-        val diff = results.filterNot(_.passed)
-
-        diff.foreach({ case Result(key, expected, got) =>
-          reporter.println(s"Expected ${expect(expected)}, but got ${actual(got)} for $key")
-        })
-
-        if (!diff.isEmpty)
-          reporter.println()
-      }
-    }
   }
 }
