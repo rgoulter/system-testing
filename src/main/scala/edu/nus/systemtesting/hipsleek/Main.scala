da84bb357e0e2c7deba1cb93041bca107ee50ce2
Richard Goulter <richard.goulter@gmail.com>
2015-09-19 17:46:52 +0800

    Code to help interact with JSON results

--- src/main/scala/edu/nus/systemtesting/hipsleek/Main.scala
+++ src/main/scala/edu/nus/systemtesting/hipsleek/Main.scala
@@ -9,6 +9,85 @@ import edu.nus.systemtesting.testsuite.TestSuiteResult
 import edu.nus.systemtesting.serialisation.TestSuiteResultJson
 import edu.nus.systemtesting.FileSystemUtilities
 import org.joda.time.format.ISODateTimeFormat
+import edu.nus.systemtesting.testsuite.TestSuiteResult
+import edu.nus.systemtesting.testsuite.TestSuiteResult
+import scala.io.Source
+
+class Results(val resultsDir: String = "results") {
+  /**
+   * Map of `(name, rev) => File`.
+   * `rev` expected to be 'short'. (12 chars).
+   */
+  private lazy val resultFiles = {
+    val filesInDir = Paths.get(resultsDir).toFile().listFiles()
+
+    // cf. filenameForSuiteResult
+    val ResultNameRegex = "(.*)-([0-9a-f]+)-(\\d+)-(\\d+)\\.json".r
+    val resultTuples = filesInDir.map({ file =>
+      file.getName() match {
+        case ResultNameRegex(name, rev, date, time) =>
+          Some((name, rev, date + time, file))
+        case _ => None
+      }
+    }).flatten
+
+    // Build map of (name, rev) => [(datetimeStr, file)]
+    val resultsMap = resultTuples.map({case (n,r,_,_) =>
+      (n,r)
+    }).toSet[(String, String)].map({ case (n,r) =>
+      val dtFilePairs = resultTuples.filter({ case (name, rev, _, _) =>
+        name == n && rev == r
+      }).map({ case (_, _, dt, f) => (dt, f) })
+
+      (n,r) -> dtFilePairs
+    }).toMap
+
+    // Take the latest result
+    // from (name,rev) => (datetimeStr, file) map
+    resultsMap.map({ case (nameRev, ls) =>
+      val (latestDateTime, file) = ls.max
+
+      nameRev -> file
+    })
+  }
+
+  def resultsFor(name: String, rev: String): Option[TestSuiteResult] = {
+    resultFiles.get((name, rev)).map({ file =>
+      val src = Source.fromFile(file)
+      val content = src.mkString
+      src.close()
+
+      TestSuiteResultJson.load(content)
+    }).flatten
+  }
+
+  /** In format of `$name-$revision-$datetime.json` */
+  private def filenameForSuiteResult(suiteResult: TestSuiteResult, name: String): String = {
+    import suiteResult.{ repoRevision, datetime }
+    val datetimeStr = datetime.toString("yyyyMMdd-HHmmss")
+
+    s"$name-${repoRevision}-${datetimeStr}.json"
+  }
+
+  /**
+   * Note for `name` that [[TestSuiteResult]] isn't necessarily from one command
+   * (`hip` or `sleek`). With the current design, convenient to consider results
+   * as 'hip results' or 'sleek results', though.
+   *
+   * @param name e.g. `"hip"`, `"sleek"`
+   */
+  def saveTestSuiteResult(suiteResult: TestSuiteResult, name: String): Unit = {
+    import suiteResult.{ repoRevision, datetime }
+
+    FileSystemUtilities.checkOutputDirectory(resultsDir)
+    val filename = filenameForSuiteResult(suiteResult, name)
+    val dump = TestSuiteResultJson.dump(suiteResult)
+
+    val path = Paths.get(resultsDir, filename)
+    reporter.log(s"\nSaving results to $path\n")
+    FileSystemUtilities.printToFile(path.toFile())(_.print(dump))
+  }
+}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -136,42 +215,34 @@ object Main {
     runTestsWith(repoDir, rev)(runPreparedHipTests)
   }
 
-  private def saveTestSuiteResult(suiteResult: TestSuiteResult, name: String): Unit = {
-    import suiteResult.{ repoRevision, datetime }
-
-    val datetimeStr = datetime.toString("yyyyMMdd-HHmmss")
-
-    // Dump to file.
-    // For now, dump to $CWD/results/
-    // (And config this later)
-    val resultsDir = "results"
-    FileSystemUtilities.checkOutputDirectory(resultsDir)
-    val filename = s"$name-${repoRevision}-${datetimeStr}.json"
-    val dump = TestSuiteResultJson.dump(suiteResult)
-
-    val path = Paths.get(resultsDir, filename)
-    reporter.log(s"\nSaving results to $path\n")
-    FileSystemUtilities.printToFile(path.toFile())(_.print(dump))
-  }
-
   /** Assumes that the project dir has been prepared successfully */
-  private def runPreparedSleekTests(projectDir: Path, revision: String): Unit = {
+  private def runPreparedSleekTests(projectDir: Path, revision: String): TestSuiteResult = {
     reporter.header("Running Sleek Tests")
 
     val significantTime = 1 // CONFIG ME
     val testCaseTimeout = 300
-    val res = new SleekTestSuiteUsage(projectDir, significantTime, testCaseTimeout, revision).run()
-    saveTestSuiteResult(res, "sleek")
+    val suite = new SleekTestSuiteUsage(projectDir, significantTime, testCaseTimeout, revision)
+
+    val res = suite.run()
+
+    (new Results).saveTestSuiteResult(res, "sleek")
+
+    res
   }
 
   /** Assumes that the project dir has been prepared successfully */
-  private def runPreparedHipTests(projectDir: Path, revision: String): Unit = {
+  private def runPreparedHipTests(projectDir: Path, revision: String): TestSuiteResult = {
     reporter.header("Running Hip Tests")
 
     val significantTime = 1 // CONFIG ME
     val testCaseTimeout = 300
-    val res = new HipTestSuiteUsage(projectDir, significantTime, testCaseTimeout, revision).run()
-    saveTestSuiteResult(res, "hip")
+    val suite = new HipTestSuiteUsage(projectDir, significantTime, testCaseTimeout, revision)
+
+    val res = suite.run()
+
+    (new Results).saveTestSuiteResult(res, "hip")
+
+    res
   }
 
   private def runSVCompTests(): Unit = {
