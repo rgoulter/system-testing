ffabef9d902efba037246e064f7b3f8f5a0666b7
Richard Goulter <richard.goulter@gmail.com>
2015-09-22 12:08:40 +0800

    ST Usage close enough to HipTestSuiteUsage

--- src/main/scala/edu/nus/systemtesting/hipsleek/SuiteGenerator.scala
+++ src/main/scala/edu/nus/systemtesting/hipsleek/SuiteGenerator.scala
@@ -1,7 +1,9 @@
 package edu.nus.systemtesting.hipsleek
 
-import org.stringtemplate.v4.STGroupString
+import scala.beans.BeanProperty
 import scala.io.Source
+
+import org.stringtemplate.v4.STGroupString
 
 /**
  * Used for generating Scala files, in tandem with [[RunFastTests]] class.
@@ -9,23 +11,57 @@ import scala.io.Source
  * @author richardg
  */
 object SuiteGenerator {
+  /**
+   * For assisting with writing the test generation.
+   *
+   * @param args may be null
+   */
+  private class Test(@BeanProperty val command: String,
+                     @BeanProperty val args: String,
+                     @BeanProperty val filename: String)
+
+  private class TestSet(@BeanProperty val name: String,
+                        @BeanProperty val tests: Array[Test])
+
+  private val sampleTestSets = {
+    // Sample
+    val stTests = Array(new Test("cmd", "", "file1"),
+                        new Test("cmd", "args", "file2"),
+                        new Test("cmd", "", "file3"))
+    val stTestSets = Array(new TestSet("example", stTests),
+                           new TestSet("example", stTests))
+
+    stTestSets
+  }
+
+  def renderSuiteTemplate(): String = {
+    val TemplateGroup = "TestSuiteUsage.stg"
+    val tgIS = getClass.getClassLoader.getResourceAsStream(TemplateGroup)
+    assert (tgIS != null)
+
+    val tgContent = Source.fromInputStream(tgIS).mkString
+    val testSuiteSTG = new STGroupString(tgContent)
+
+    val template = testSuiteSTG.getInstanceOf("suite"); 
+
+    val stTestSets = sampleTestSets
+
+    template.add("name", "Hello")
+    template.add("pkg", "world")
+    template.add("examplesDir", "examples/sample")
+    template.add("command", "myCmd")
+    template.add("testSets", stTestSets)
+
+    template.render()
+  }
+
+  def foo(x: Int) = println(x)
+  def foo(x: Int, y: Int) = println(x)
+
   def main(args: Array[String]): Unit = {
-  val TemplateGroup = "TestSuiteUsage.stg"
-  val tgIS = getClass.getClassLoader.getResourceAsStream(TemplateGroup)
-  assert (tgIS != null)
-  val tgContent = Source.fromInputStream(tgIS).mkString
-  val testSuiteSTG = new STGroupString(tgContent)
+    val output = renderSuiteTemplate
 
-
-  val template = testSuiteSTG.getInstanceOf("suite"); 
-
-  template.add("name", "Hello")
-  template.add("pkg", "world")
-  template.add("files", Array("f1", "f2", "f3"))
-
-  val output = template.render()
-
-  println(output)
+    println(output)
   }
 }
 
