1bd7cb3b950ca9e9b670ed25cd65f8d9601ea7dd
Richard Goulter <richard.goulter@gmail.com>
2015-09-29 19:00:50 +0800

    HG add commonAncestor, branchOf, commitsInRange methods

--- src/main/scala/edu/nus/systemtesting/hg/Repository.scala
+++ src/main/scala/edu/nus/systemtesting/hg/Repository.scala
@@ -45,6 +45,18 @@ class Repository(dir: Path) {
       throw new UnknownRevisionException(rev.getOrElse("<head>"), dir)
   }
 
+  def branchOf(rev: Option[String] = None): String = {
+    val cmd = "hg identify -b" + rev.map(r => s" -r $r").getOrElse("")
+    val proc = Process(cmd, repoDir)
+
+    val execOutp = Runnable.executeProc(proc)
+
+    if (execOutp.exitValue == 0)
+      execOutp.output.trim()
+    else
+      throw new UnknownRevisionException(rev.getOrElse("<head>"), dir)
+  }
+
   /**
    * Returns list of (short) revision hashes of the parent(s) of the given
    * revision.
@@ -61,6 +73,66 @@ class Repository(dir: Path) {
       execOutp.output.trim().lines.toList
     else
       throw new UnknownRevisionException(rev.getOrElse("<head>"), dir)
+  }
+
+  def commonAncestor(rev1: String, rev2: String): String = {
+    // check rev1, rev2 are actually in the repo. These throw ex. if not
+    require(identify(Some(rev1)) != "")
+    require(identify(Some(rev2)) != "")
+
+    // I'm not sure what happens if e.g. rev1, rev2 not related
+    val cmd = s"hg debugancestor $rev1 $rev2"
+    val proc = Process(cmd, repoDir)
+
+    val execOutp = Runnable.executeProc(proc)
+
+    if (execOutp.exitValue == 0) {
+      // output of debugancestor is like:
+      //   16470:45c49c7e0321316504d10f1202aa0f58cfddd840
+      // can either pass to identify(), or just trim + take 2nd.
+      val res = execOutp.output.trim()
+      res.split(":")(1)
+    } else {
+      throw new IllegalStateException
+    }
+  }
+
+  /**
+   * Returns a List of all the commits in the range `rev1...rev2`,
+   * on the same branch.
+   *
+   * Assumes rev1, rev2 both valid revisions, and one is ancestor of the other,
+   * and must be on the same branch.
+   */
+  def commitsInRange(rev1: String, rev2: String): List[String] = {
+    // check rev1, rev2 are actually in the repo. These throw ex. if not
+    require(identify(Some(rev1)) != "")
+    require(identify(Some(rev2)) != "")
+
+    val oldest = commonAncestor(rev1, rev2)
+    val newest = if (oldest startsWith rev1) {
+      rev2
+    } else if (oldest startsWith rev2) {
+      rev1
+    } else {
+      throw new IllegalArgumentException(s"Revisions must be related in linear manner")
+    }
+
+    val branch1 = branchOf(Some(rev1))
+    val branch2 = branchOf(Some(rev2))
+
+    require(branch1 == branch2)
+
+    // I'm not sure what happens if e.g. rev1, rev2 not related
+    val cmd = s"""hg log --template={node|short}\\n -r $oldest:$newest -b $branch1"""
+    val proc = Process(cmd, repoDir)
+
+    val execOutp = Runnable.executeProc(proc)
+
+    if (execOutp.exitValue == 0)
+      execOutp.output.trim().lines.toList
+    else
+      throw new IllegalStateException
   }
 
   def isDirty(): Boolean = {
@@ -93,4 +165,23 @@ class Repository(dir: Path) {
       (parts(0), parts(1))
     })
   }
+}
+
+/** For debugging purposes */
+private object RepositoryApp extends App {
+  // range of commits 45c49c:79da9 should be like:
+  //   45/40/c6/24/cc/ad/7a/4b/ce/80/79
+//  val rev1 = "45c49c"
+//  val rev2 = "79da9"
+  //   01/f6/0b/c6/d1/89/0c/f5/fd/ff/a5/3b/f5
+  val rev1 = "0111c"
+  val rev2 = "f59ebb"
+
+  // Hardcoded, but that's fine.
+  import java.nio.file.Paths
+  val repo = new Repository(Paths.get("/home/richardg/hg/sleekex"))
+
+  val range = repo.commitsInRange(rev1, rev2)
+  println(range)
+  println(range map { s => s.substring(0, 2) } mkString("/"))
 }
