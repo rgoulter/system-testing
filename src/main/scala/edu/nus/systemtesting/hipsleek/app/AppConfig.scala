3b7c4a0e36b00c83ad73b67c8d9b06fe3f866b4a
Richard Goulter <richard.goulter@gmail.com>
2015-11-27 18:45:51 +0800

    v0.6 release

--- src/main/scala/edu/nus/systemtesting/hipsleek/app/AppConfig.scala
+++ src/main/scala/edu/nus/systemtesting/hipsleek/app/AppConfig.scala
@@ -196,7 +196,7 @@ object AppConfig {
   val CommandLineOptionsParser = new scopt.OptionParser[AppConfig]("system-tests") {
     import VisibilityOptions.ShowANSI
 
-    head("run-system-tests", "0.6.0-SNAPSHOT")
+    head("run-system-tests", "0.6.0")
     help("help") text("prints this usage text")
     version("version")
     opt[Int]('t', "timeout") action { (x, c) =>
