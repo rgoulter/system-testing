68ae172f6b1c76f7436e0956e6caf373d130c963
Richard Goulter <richard.goulter@gmail.com>
2016-01-27 18:02:17 +0800

    Update script, add notes on recommendations from analysis

--- scripts/results-analyse-time.py
+++ scripts/results-analyse-time.py
@@ -43,12 +43,20 @@ def load_json_file(json_path):
 
 
 
+timeof = lambda (time,kind): time
+kindof = lambda (time,kind): kind
+
+
+
 def filter_for_kind(kind):
 	return lambda (xs) : filter(lambda (t,k): k == kind, xs)
 
 filter_for_timeout = filter_for_kind("TIMEOUT")
 filter_for_invalid = filter_for_kind("INVALID")
 filter_for_valid = filter_for_kind("VALID")
+
+def filter_for_time(time, xs):
+	return filter(lambda (t,k): t <= time, xs)
 
 
 
@@ -80,21 +88,27 @@ def chart_results(ls_of_res):
 	def percentile_idx(p):
 		return int(math.ceil(float(len(sorted_data) * p) / 100)) - 1
 
-	timeof = lambda (time,kind): time
-	kindof = lambda (time,kind): kind
-
-	def print_stats(xs):
+	def print_stats(xs, use_timeout = -1):
 		# Longest Running
-		print "Longest Time:         %8d ms" % timeof(xs[-1])
+		longest = timeof(xs[-1])
+		print "Longest Time:            %8d ms" % longest
 
 		# Sum up till that point
 		total = sum([timeof(d) for d in xs])
-		print "Total Time:           %8d ms" % total
+		print "Total Time (excl T/OUT): %8d ms" % total
+		tout = max(use_timeout, longest) # use use_timeout, if given, otherwise use `longest`
+		touts = total + (tout * (len(sorted_data) - len(xs)))
+		print "Total Time (incl T/OUT): %8d ms" % touts
 
+
+		# What % of all tests are "covered" by `xs`?
 		print "Percent of All Tests:     %3d%%" % (100 * float(len(xs)) / len(sorted_data))
+		# How many aren't?
+		print "Complement size (T/OUT):  %d" % (len(sorted_data) - len(xs))
 
 		print "Percent of VALID   tests: %3d%%" % (100 * float(len(filter_for_valid(xs))) / len(sorted_valid_data))
 		print "Percent of INVALID tests: %3d%%" % (100 * float(len(filter_for_invalid(xs))) / len(sorted_invalid_data))
+
 
 
 	print "Charting Results"
@@ -102,14 +116,25 @@ def chart_results(ls_of_res):
 	print
 
 	# percentiles 10..90
-	for percent in range(10, 100, 10):
+	# n.b. ostensible < 80-percentile not great, so
+	for percent in range(80, 100, 5):
 		print "For %d Percentile:" % percent
 
 		pidx = percentile_idx(percent)
 		xs = sorted_data[:pidx+1]
 
 		print_stats(xs)
+		print
 
+	# Want to investigate various proposed T/OUT times.
+	# The largest not-TIMEOUT-time is ~138s, which would surely cost too much.
+	# for time in range(1800, 3200, 200):
+	for time in range(1500, 6000, 500):
+		print "For tentative timeout %d ms:" % time
+
+		xs = filter_for_time(time, sorted_data)
+
+		print_stats(xs, time)
 		print
 
 	# For reference, if we do *all* tests:
@@ -122,4 +147,11 @@ if __name__ == "__main__":
 	dirname = sys.argv[1]
 
 	res = load_files_in_dir(dirname)
+
 	chart_results(res)
+	print
+
+	# Find longest time, before timeout.
+	# i.e. if time longer than this, 100% chance it will time out.
+	# (if this is >> 2s, then will be expensive to run all tests..)
+	print "Count > 300s: %d" % len(filter_for_timeout(res))
